4a6987ef5e5fcd4fb3fff37f4a1b23379a220f15
Vishal kumar <vishal.kumar@prooh.ai>
2024-11-29 20:21:13 +0530

    Added search screens by screen name on screen list page

--- src/pages/ScreensPage/MiddleArea.tsx
+++ src/pages/ScreensPage/MiddleArea.tsx
@@ -11,34 +11,34 @@ import { Loading } from "../../components/Loading";
 import { getDataFromLocalStorage } from "../../utils/localStorageUtils";
 import { ALL_SCREENS_LIST } from "../../constants/localStorageConstants";
 import { getTimeDifferenceInMin } from "../../utils/dateAndTimeUtils";
-
+import SearchInputField from "../../components/molecules/SearchInputField";
 
 export const MiddleArea: React.FC = () => {
   const dispatch = useDispatch<any>();
   const navigate = useNavigate();
   const targetDivRef = useRef<HTMLDivElement>(null);
+  const [searchText, setSearchText] = useState<string>("");
 
   const [selectedCard, setSelectedCard] = useState<any>(null);
 
   const auth = useSelector((state: any) => state.auth);
   const { userInfo } = auth;
 
-  const allScreensDataGet = useSelector((state: any) => state.allScreensDataGet);
-  const {
-    loading, error, data: allScreens
-  } = allScreensDataGet;
+  const allScreensDataGet = useSelector(
+    (state: any) => state.allScreensDataGet
+  );
+  const { loading, error, data: allScreens } = allScreensDataGet;
 
   useEffect(() => {
     if (userInfo && !userInfo?.isMaster) {
-      message.error("Not a screen owner!!!")
+      message.error("Not a screen owner!!!");
     }
 
     if (!userInfo) {
-      navigate("/auth")
+      navigate("/auth");
     }
-    dispatch(getAllScreensDetailsAction({userId: userInfo?._id}));
-  },[dispatch, userInfo]);
-
+    dispatch(getAllScreensDetailsAction({ userId: userInfo?._id }));
+  }, [dispatch, userInfo]);
 
   // Handle card click, setting the clicked card's index
   const handleCardClick = (id: any) => {
@@ -51,12 +51,33 @@ export const MiddleArea: React.FC = () => {
         {loading ? (
           <Loading />
         ) : (
-          <div className="flex gap-4 items-center flex-wrap">
-            {getDataFromLocalStorage(ALL_SCREENS_LIST)?.list?.map((data: any, index: any) => (
-                <div key={index} className="">
-                  <ScreenListThumbnail isSelected={data._id === selectedCard} color={""} handleCardClick={() => handleCardClick(data._id)} navigate={() => navigate(`/screens-details/${data._id}`)} data={data}/>
-                </div>
-              ))}
+          <div className="">
+            <div className="w-[34vw] pb-4">
+              <SearchInputField
+                value={searchText}
+                onChange={setSearchText}
+                placeholder="Search by screen name"
+              />
+            </div>
+            <div className="flex gap-8 items-center flex-wrap">
+              {getDataFromLocalStorage(ALL_SCREENS_LIST)
+                ?.list?.filter((screen: any) =>
+                  screen.screenName
+                    .toLowerCase()
+                    .includes(searchText?.toLowerCase())
+                )
+                ?.map((data: any, index: any) => (
+                  <div key={index} className="">
+                    <ScreenListThumbnail
+                      isSelected={data._id === selectedCard}
+                      color={""}
+                      handleCardClick={() => handleCardClick(data._id)}
+                      navigate={() => navigate(`/screens-details/${data._id}`)}
+                      data={data}
+                    />
+                  </div>
+                ))}
+            </div>
           </div>
         )}
       </div>
