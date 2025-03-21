9930bb2fbcef97b71a8004bd15e6b752784125f5
Ansh <anshtandonat@gmail.com>
2025-03-21 14:46:14 +0530

    changed css to show delivered campaign seperately from paused

--- src/components/molecules/BrandCampaignScreenDetails.tsx
+++ src/components/molecules/BrandCampaignScreenDetails.tsx
@@ -11,22 +11,23 @@ export function BrandCampaignScreenDetails({
   index,
   currentTab,
 }: any) {
-  const creativeNameDay = campaign?.creatives?.standardDayTimeCreatives?.[0]?.url.split("/")[campaign?.creatives?.standardDayTimeCreatives[0]?.url.split("/")?.length-1];
-  const creativeNameNight = campaign?.creatives?.standardNightTimeCreatives?.[0]?.url.split("/")[campaign?.creatives?.standardNightTimeCreatives[0]?.url.split("/")?.length-1];
-  const creativeNameTrigger = campaign?.creatives?.triggerCreatives?.[0]?.url.split("/")[campaign?.creatives?.triggerCreatives[0]?.url.split("/")?.length-1];
+  const creativeNameDay = campaign?.creatives?.standardDayTimeCreatives?.[0]?.url.split("/")[campaign?.creatives?.standardDayTimeCreatives[0]?.url.split("/")?.length - 1];
+  const creativeNameNight = campaign?.creatives?.standardNightTimeCreatives?.[0]?.url.split("/")[campaign?.creatives?.standardNightTimeCreatives[0]?.url.split("/")?.length - 1];
+  const creativeNameTrigger = campaign?.creatives?.triggerCreatives?.[0]?.url.split("/")[campaign?.creatives?.triggerCreatives[0]?.url.split("/")?.length - 1];
   const getBgColors = (index: any) => {
-    const colors = ["bg-[#EF444450]", "bg-[#F59E0B50]", "bg-[#EAB30850]", "bg-[#22C55E50]", "bg-[#06B6D450]", "bg-[#3B82F650]", "bg-[#6366F150]", "bg-[#8B5CF650]", "bg-[#78DCCA50]", "bg-[#FF77E950]", "bg-[#3AB7BF50]", "bg-[#3F3CBB50]", "bg-[#22C55E50]", "bg-[#06B6D450]", "bg-[#3B82F650]", "bg-[#6366F150]", "bg-[#EF444450]", "bg-[#F59E0B50]" ];
+    const colors = ["bg-[#EF444450]", "bg-[#F59E0B50]", "bg-[#EAB30850]", "bg-[#22C55E50]", "bg-[#06B6D450]", "bg-[#3B82F650]", "bg-[#6366F150]", "bg-[#8B5CF650]", "bg-[#78DCCA50]", "bg-[#FF77E950]", "bg-[#3AB7BF50]", "bg-[#3F3CBB50]", "bg-[#22C55E50]", "bg-[#06B6D450]", "bg-[#3B82F650]", "bg-[#6366F150]", "bg-[#EF444450]", "bg-[#F59E0B50]"];
     return colors[index];
   }
 
   return (
     <div className="group relative my-1">
       <div
-        className={`${
-          campaignIds?.includes(campaign?._id)
-            ? "border border-red-500 rounded"
+        className={`${campaignIds?.includes(campaign?._id)
+          ? "border border-red-500 rounded"
+          : campaign?.status === "Delivered"
+            ? "border border-primary rounded"
             : ""
-        } flex p-2 gap-4 hover:bg-gray-100 hover:rounded`}
+          } flex p-2 gap-4 hover:bg-gray-100 hover:rounded`}
       >
         <div
           className={`rounded flex justify-center items-center w-20 ${getBgColors(campaign?.brandName?.split(" ")[0]?.split("")?.length)}`}
@@ -37,9 +38,14 @@ export function BrandCampaignScreenDetails({
         </div>
         <div className="truncate flex flex-col gap-1">
           <div className="flex items-center justify-start gap-2">
-            <h1 className="text-[14px] font-semibold truncate">
-              {campaign?.name}
-            </h1>
+            <div className="flex items-center gap-2">
+              <h1 className="text-[14px] font-semibold truncate">
+                {campaign?.name}
+              </h1>
+              {campaign?.status === "Delivered" && <h1 className="text-[12px] text-primaryButton truncate">
+                {campaign?.status}
+              </h1>}
+            </div>
             {["1", "2"]?.includes(currentTab) && (
               <div className="flex items-center justify-start gap-2">
                 {creativeNameDay && (
@@ -54,7 +60,7 @@ export function BrandCampaignScreenDetails({
               </div>
             )}
 
-            
+
           </div>
 
           <p className="text-[10px] truncate">{brandName}</p>
@@ -63,7 +69,7 @@ export function BrandCampaignScreenDetails({
               <i className="text-[10px] fi fi-rr-calendar-clock pr-1 flex items-center justify-center"></i>
               <p className="text-[10px] truncate">{campaign?.campaignDuration} days</p>
             </div>
-            
+
           )}
           {/* group-hover:opacity-0 transition-opacity duration-300 */}
           <div className="flex gap-2 text-[12px]">
