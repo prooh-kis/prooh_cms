969ef870773bf6dfa6fd0221303558a24bfc6699
Ansh <anshtandonat@gmail.com>
2025-06-21 16:29:43 +0530

    resolved a bug

--- src/constants/urlConstants.js
+++ src/constants/urlConstants.js
@@ -1,11 +1,15 @@
-export const screenV2 = `${process.env.REACT_APP_PROOH_SERVER}/api/v2/screens`;
-export const campaignV2 = `${process.env.REACT_APP_PROOH_SERVER}/api/v2/campaigns`;
-export const creativeV2 = `${process.env.REACT_APP_PROOH_SERVER}/api/v2/creatives`;
-export const userV1 = `${process.env.REACT_APP_PROOH_SERVER}/api/v1/users`;
-export const queriesV2 = `${process.env.REACT_APP_PROOH_SERVER}/api/v2/landing`;
+const cleanUrl = (path) => {
+    const baseUrl = process.env.REACT_APP_PROOH_SERVER?.replace(/[\"\']/g, '').replace(/\/$/, '');
+    return `${baseUrl}${path}`;
+};
 
-export const analyticsV1 = `${process.env.REACT_APP_PROOH_SERVER}/api/v1/analytics`;
-export const monitoringV2 = `${process.env.REACT_APP_PROOH_SERVER}/api/v2/monitoring`;
+export const queriesV2 =cleanUrl('/api/v2/landing')
+export const monitoringV2 =cleanUrl('api/v2/monitoring')
+export const screenV2 = cleanUrl('/api/v2/screens');
+export const campaignV2 = cleanUrl('/api/v2/campaigns');
+export const creativeV2 = cleanUrl('/api/v2/creatives');
+export const userV1 = cleanUrl('/api/v1/users');
+export const analyticsV1 = cleanUrl('/api/v1/analytics');
 
 export const cmsURL = "https://prooh-cms.vercel.app/sign-in";
 export const planningURL = "https://plan.prooh.ai/sign-up";
